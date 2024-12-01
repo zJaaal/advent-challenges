a3a349440f17bc4d753c7dec6a57362d0332ff8e
Jalinson Diaz <zjaaaldev@gmail.com>
2024-12-01 02:30:05 -0300

    advent of code day 1

--- 2024/adventOfCode/1_day/1_part/solution.js
+++ 2024/adventOfCode/1_day/1_part/solution.js
@@ -3,11 +3,31 @@ console.clear();
 console.log('----------------------------------');
 
 /*
- Add useful description
+  With the 2 lists, sort them in ascending order and calculate the sum of the absolute differences between the elements of the 2 lists.
  */
 
 function solution(input) {
-  return input;
+  const { left, right } = input.split('\n').reduce(
+    (acc, pair) => {
+      const [left, right] = pair.split('   ');
+
+      return {
+        left: [...acc.left, left],
+        right: [...acc.right, right],
+      };
+    },
+    {
+      left: [],
+      right: [],
+    }
+  );
+
+  const sortedLeft = left.sort((a, b) => a - b);
+  const sortedRight = right.sort((a, b) => a - b);
+
+  return sortedLeft.reduce((acc, item, index) => {
+    return acc + Math.abs(item - sortedRight[index]);
+  }, 0);
 }
 
 console.log(solution(input));
--- 2024/adventOfCode/1_day/2_part/solution.js
+++ 2024/adventOfCode/1_day/2_part/solution.js
@@ -3,11 +3,40 @@ console.clear();
 console.log('----------------------------------');
 
 /*
- Add useful description
+  After the previous task, out protagonist found out that we needed to calculate a similarity score between the 2 lists
+  We need to multiply each number by the number of times it appears in the other list and sum the results.
  */
 
 function solution(input) {
-  return input;
+  const { left, right } = input.split('\n').reduce(
+    (acc, pair) => {
+      const [left, right] = pair.split('   ');
+
+      return {
+        left: {
+          ...acc.left,
+          [left]: acc.left[left] ? acc.left[left] + 1 : 1, // Get all the numbers from the left list and count how many times they appear
+        },
+        right: {
+          ...acc.right,
+          [right]: acc.right[right] ? acc.right[right] + 1 : 1, // Get all the numbers from the right list and count how many times they appear
+        },
+      };
+    },
+    {
+      left: {},
+      right: {},
+    }
+  );
+
+  return Object.entries(left).reduce((acc, item) => {
+    const [leftId, leftCount] = item;
+
+    const rightCount = right[leftId] ?? 0; // If the number in the left list is not in the right list, we need to consider it as 0
+
+    // Multiply the left number by the times it appears in the right list, but we need to take into consideration that the number in the left can be repeated also in the left list
+    return acc + leftCount * Number(leftId) * rightCount;
+  }, 0);
 }
 
 console.log(solution(input));
