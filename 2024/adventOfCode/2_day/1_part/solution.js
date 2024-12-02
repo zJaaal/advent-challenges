84432654da64061fc86ad1584354eead992acaa0
Jalinson Diaz <zjaaaldev@gmail.com>
2024-12-02 11:01:06 -0300

    finally day 2

--- 2024/adventOfCode/2_day/1_part/solution.js
+++ 2024/adventOfCode/2_day/1_part/solution.js
@@ -3,12 +3,36 @@ console.clear();
 console.log('----------------------------------');
 
 /*
- Add useful description
+The engineers are trying to figure out which reports are safe.
+The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing.
+So, a report only counts as safe if both of the following are true:
+
+The levels are either all increasing or all decreasing.
+Any two adjacent levels differ by at least one and at most three.
  */
 
 function solution(input) {
-  return input;
+  const reports = input.split('\n').map((line) => line.split(' ').map(Number));
+
+  return reports.filter((report) => {
+    let direction = 0;
+
+    return report.every((level, index) => {
+      if (index === 0) return true;
+      const prevLevel = report[index - 1];
+
+      const currentDirection = level > prevLevel ? 1 : -1;
+
+      if (direction === 0) direction = level > prevLevel ? 1 : -1;
+
+      const difference = Math.abs(level - prevLevel);
+
+      return (
+        difference <= 3 && difference > 0 && currentDirection === direction
+      );
+    });
+  }).length;
 }
 
-// console.log(solution(input));
-console.log(solution(testInput));
+console.log(solution(input));
+// console.log(solution(testInput));
--- 2024/adventOfCode/2_day/2_part/solution.js
+++ 2024/adventOfCode/2_day/2_part/solution.js
@@ -3,12 +3,58 @@ console.clear();
 console.log('----------------------------------');
 
 /*
- Add useful description
- */
+The engineers are trying to figure out which reports are safe.
+The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing.
+So, a report only counts as safe if both of the following are true:
 
+The levels are either all increasing or all decreasing.
+Any two adjacent levels differ by at least one and at most three.
+
+Now we can have a tolerance of one wrong number
+*/
+const MAX_TOLERANCE = 1;
+
+function checkReport(report, tolerance = 0) {
+  // TIL: The direction of the sequence is obtained by comparing the first number with the last one
+  const sequenceDirection = report.at(0) > report.at(-1) ? -1 : 1;
+
+  return report.every((_, curr) => {
+    if (curr === report.length - 1) return true; // Final number edge case
+
+    const next = curr + 1;
+    const diff = Math.abs(report[next] - report[curr]);
+
+    const pairDirection = report[curr] > report[next] ? -1 : 1;
+
+    // If the difference is not between 1 and 3 or the direction is not the same
+    // We can try by removing the current or the next number
+    if (diff < 1 || diff > 3 || pairDirection !== sequenceDirection) {
+      if (tolerance == MAX_TOLERANCE) {
+        return false;
+      }
+
+      const reportWithoutCurr = report
+        .slice(0, curr)
+        .concat(report.slice(curr + 1));
+
+      const reportWithoutNext = report
+        .slice(0, next)
+        .concat(report.slice(next + 1));
+
+      return (
+        checkReport(reportWithoutCurr, tolerance + 1) ||
+        checkReport(reportWithoutNext, tolerance + 1)
+      );
+    }
+
+    return true; // The difference is between 1 and 3 and the direction is the same
+  });
+}
 function solution(input) {
-  return input;
+  const reports = input.split('\n').map((line) => line.split(' ').map(Number));
+
+  return reports.filter((report) => checkReport(report)).length;
 }
 
-// console.log(solution(input));
-console.log(solution(testInput));
+console.log(solution(input));
+// console.log(solution(testInput));
