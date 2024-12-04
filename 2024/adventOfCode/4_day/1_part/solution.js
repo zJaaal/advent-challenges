2a572c7e1600752ec57e237d1e428a90d3c7aed6
Jalinson Diaz <zjaaaldev@gmail.com>
2024-12-04 10:30:11 -0300

    advent of code day 4

---
+++ 2024/adventOfCode/4_day/1_part/solution.js
@@ -0,0 +1,58 @@
+import { input, testInput } from '../input.js';
+console.clear();
+console.log('----------------------------------');
+
+/*
+"Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the only button on it.
+After a brief flash, you recognize the interior of the Ceres monitoring station!
+
+As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her word search (your puzzle input).
+She only has to find one word: XMAS.
+
+This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words.
+It's a little unusual, though, as you don't merely need to find one instance of XMAS - you need to find all of them.
+ */
+
+function solution(input) {
+  let count = 0;
+  const lettersMatrix = input.split('\n').map((row) => row.split(''));
+
+  for (let rowIndex = 0; rowIndex < lettersMatrix.length; rowIndex++) {
+    for (
+      let letterIndex = 0;
+      letterIndex < lettersMatrix[rowIndex].length;
+      letterIndex++
+    ) {
+      const letter = lettersMatrix[rowIndex][letterIndex];
+
+      // Skip if not X
+      if (letter !== 'X') continue;
+
+      // Check all 8 directions from X
+      const directions = {
+        right: [0, 1],
+        left: [0, -1],
+        up: [-1, 0],
+        down: [1, 0],
+        upLeft: [-1, -1],
+        upRight: [-1, 1],
+        downLeft: [1, -1],
+        downRight: [1, 1],
+      };
+
+      const words = Object.values(directions).map(
+        ([rowDelta, colDelta]) =>
+          letter +
+          lettersMatrix[rowIndex + rowDelta]?.[letterIndex + colDelta] +
+          lettersMatrix[rowIndex + 2 * rowDelta]?.[letterIndex + 2 * colDelta] +
+          lettersMatrix[rowIndex + 3 * rowDelta]?.[letterIndex + 3 * colDelta]
+      );
+
+      // Count XMAS occurrences
+      count += words.filter((word) => word === 'XMAS').length;
+    }
+  }
+  return count;
+}
+console.log(solution(input));
+// console.log(solution(testInput));
---
+++ 2024/adventOfCode/4_day/2_part/solution.js
@@ -0,0 +1,72 @@
+import { input, testInput } from '../input.js';
+console.clear();
+console.log('----------------------------------');
+
+/*
+Looking for the instructions, you flip over the word search to find that this
+isn't actually an XMAS puzzle; it's an X-MAS puzzle in which you're supposed
+to find two MAS in the shape of an X.
+
+One way to achieve that is like this:
+
+M.S
+.A.
+M.S
+*/
+
+function solution(input) {
+  const lettersMatrix = input.split('\n').map((row) => row.split(''));
+  let count = 0;
+  for (let rowIndex = 0; rowIndex < lettersMatrix.length; rowIndex++) {
+    for (
+      let letterIndex = 0;
+      letterIndex < lettersMatrix[rowIndex].length;
+      letterIndex++
+    ) {
+      const letter = lettersMatrix[rowIndex][letterIndex];
+
+      // If the letter is not an A, we can skip the current iteration
+      if (letter !== 'A') continue;
+
+      // Get the corners of the current letter, which should be A
+      const corners = [
+        {
+          letter: lettersMatrix[rowIndex - 1]?.[letterIndex - 1],
+          row: rowIndex - 1,
+          col: letterIndex - 1,
+        },
+        {
+          letter: lettersMatrix[rowIndex - 1]?.[letterIndex + 1],
+          row: rowIndex - 1,
+          col: letterIndex + 1,
+        },
+        {
+          letter: lettersMatrix[rowIndex + 1]?.[letterIndex - 1],
+          row: rowIndex + 1,
+          col: letterIndex - 1,
+        },
+        {
+          letter: lettersMatrix[rowIndex + 1]?.[letterIndex + 1],
+          row: rowIndex + 1,
+          col: letterIndex + 1,
+        },
+      ];
+
+      // Filter the corners that are M and S
+      const lettersM = corners.filter((corner) => corner.letter === 'M');
+      const lettersS = corners.filter((corner) => corner.letter === 'S');
+
+      // If we don't have at least two M and two S, we can skip the current iteration
+      if (lettersM.length < 2 || lettersS.length < 2) continue;
+
+      // If the two M are adjacent then we can count the XMAS
+      count += Number(
+        lettersM[0].row === lettersM[1].row ||
+          lettersM[0].col === lettersM[1].col
+      );
+    }
+  }
+  return count;
+}
+console.log(solution(input));
+// console.log(solution(testInput));
